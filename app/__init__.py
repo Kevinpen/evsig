d4c3410912a1aec87b2df12aafb1cda5798f8ce3
Kevinpen <khazar@163.com>
2021-05-16 00:14:42 -0400

    seperate views

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
-from .main import main as main_blueprint
-from .main import sig as sig_blueprint
+from .main import main,choose,result
+
 #from config import config
 
 def create_app(config_name):
  app = Flask(__name__)
- 
- app.register_blueprint(main_blueprint)
- app.register_blueprint(sig_blueprint)
- 
+ app.register_blueprint(result)
+ app.register_blueprint(choose)
+ app.register_blueprint(main)
+
 
  return app
